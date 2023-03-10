adae258554ccf1330cc11f35d5e17ff10309e211
Kasun Miuranga <kasunmiuranga2000@gmail.com>
2023-03-10 18:40:48 +0530

    Initial Commit

---
+++ src/lk/ijse/thogakadepos/util/SessionFactoryConfiguration.java
@@ -0,0 +1,26 @@
+package lk.ijse.thogakadepos.util;
+
+import lk.ijse.thogakadepos.entity.Customer;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class SessionFactoryConfiguration {
+    private final SessionFactory sessionFactory;
+    private static SessionFactoryConfiguration sessionFactoryConfiguration;
+
+    private SessionFactoryConfiguration(){
+        sessionFactory = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Customer.class).buildSessionFactory();
+    }
+
+    public static SessionFactoryConfiguration getInstance(){
+        return sessionFactoryConfiguration == null ?
+                sessionFactoryConfiguration = new SessionFactoryConfiguration() : sessionFactoryConfiguration;
+    }
+
+    public Session getSession() throws HibernateException {
+        Session session = sessionFactory.openSession();
+        return session;
+    }
+}
